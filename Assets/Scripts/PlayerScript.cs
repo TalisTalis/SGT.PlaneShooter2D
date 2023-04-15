900607532242db269204158f560703f1704a83fe
Stanislav <tsarst@gmail.com>
2023-04-15 07:44:31 +0300

    input player

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float speed = 5f;
     void Update()
     {
-        
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float newXpos = transform.position.x + deltaX;
+        float newYpos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXpos, newYpos);
     }
 }
