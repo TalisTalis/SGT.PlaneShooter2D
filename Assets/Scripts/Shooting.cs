4972f8097f8068cf3c330bd1b8596e71ac9354e3
Stanislav <tsarst@gmail.com>
2023-04-15 16:44:32 +0300

    autoshoot coroutine

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
@@ -5,13 +6,32 @@ public class Shooting : MonoBehaviour
     public GameObject playerBullet;
     public Transform spawnPoint1;
     public Transform spawnPoint2;
+    public float bulletSpawnTime = 1f;
 
+    private void Start()
+    {
+        StartCoroutine(Shoot());
+    }
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    Fire();
+        //}
+    }
+
+    void Fire()
+    {
+        Instantiate(playerBullet, spawnPoint1.position, Quaternion.identity);
+        Instantiate(playerBullet, spawnPoint2.position, Quaternion.identity);
+    }
+
+    IEnumerator Shoot()
+    {
+        while (true)
         {
-            Instantiate(playerBullet, spawnPoint1.position, Quaternion.identity);
-            Instantiate(playerBullet, spawnPoint2.position, Quaternion.identity);
+            yield return new WaitForSeconds(bulletSpawnTime);
+            Fire();
         }
     }
 }
